3bed7b7703035efba2cfc6b8914a0610d9ec1e14
amine <amine@gmail.com>
2019-05-21 15:49:56 +0200

    first commit

---
+++ src/main/java/org/sid/GestionStockServerApplication.java
@@ -0,0 +1,13 @@
+ package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockServerApplication.class, args);
+	}
+
+}
